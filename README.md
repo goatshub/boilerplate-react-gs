78960b8e4cbd666c982c37d343cbcfefe7bfc8f9
goatshub <kiatwongwanich@gmail.com>
2023-02-14 13:20:25 +0700

    No React Router

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 boilerplate-gs-react
 
-init clasp to set up google script and then run parcel
+init clasp to set up google script and then run parcel. 
+
 Use npm start to compile react and use npm run gstart to watch for update
--- apps-script/main.js
+++ apps-script/main.js
@@ -1,4 +1,4 @@
 function doGet(){
     let tmp = HtmlService.createTemplateFromFile("index")
-    return tmp.evaluate().setTitle("Webpage").addMetaTag("viewport", "width-device-width, initial-scale=1.0")
+    return tmp.evaluate().setTitle("<Title>").addMetaTag("viewport", "width-device-width, initial-scale=1.0")
 }
